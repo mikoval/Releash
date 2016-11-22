883ef221e5c37a19389900ba386ffaaad28a1d1a
Michael Koval <mike@Michaels-MacBook-Pro-2.local>
2016-11-22 12:54:57 -0500

    added default user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
 #  this is where you put in values that you want in the database when created. 
 # run 'rake db:seed'
 Role.create([{title: 'Volunteer'}, {title: 'Employee'}, {title: 'Administrator '}])
+User.create ({name: "Admin", email: "admin@releash.com", password: "password", password_confirmation: "password", role_id:3})
