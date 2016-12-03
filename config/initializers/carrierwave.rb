d96f73dcdbec6992e203215cff2c8675d8124eea
Michael Koval <mike@Michaels-MacBook-Pro-2.local>
2016-12-03 00:33:03 -0500

    changed carriorwave file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider              => 'AWS',
-    :aws_access_key_id     => ENV['AKIAIOFRK67VFYY7P5UQ'],
-    :aws_secret_access_key => ENV['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'],
-                      # required
+   :provider               => 'AWS', 
+    :s3_access_key_id      => ENV['S3_ACCESS_KEY'], 
+    :s3_secret_access_key  => ENV['S3_SECRET_KEY']
+                    
   }
-  config.fog_directory  = ENV["releash"]                     # required
+  config.fog_directory  = ENV["releash"]  
 end
