b87a7fd62f0c8c748ae3cde119654795188afa90
Michael Koval <mike@michaels-mbp-2.attlocal.net>
2017-02-12 10:38:26 -0500

    fixd created animal breeds migration

--- db/migrate/20170212040147_create_animal_breeds.rb
+++ db/migrate/20170212040147_create_animal_breeds.rb
@@ -1,6 +1,5 @@
 class CreateAnimalBreeds < ActiveRecord::Migration
   def change
-    drop_table :animal_breeds
     create_table :animal_breeds do |t|
       t.integer :animal_id
       t.integer :breed_id
