ffd6339d70f84fab178a1442aca1010bc95fde03
Michael Koval <mike@Michaels-MacBook-Pro-2.local>
2017-03-15 16:39:59 -0400

    forgot to remove filter on activation

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,5 +1,5 @@
 class AccountActivationsController < ApplicationController
-
+  skip_before_filter :require_login
   def edit
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
