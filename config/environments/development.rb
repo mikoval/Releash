43ccf700b6c271a57a0215d1497bb83b7922b551
Michael Koval <mike@Michaels-MacBook-Pro-2.local>
2017-03-15 16:36:00 -0400

    changing host info

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,6 +40,6 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'ishelter.com' # Don't use this literally; use your local dev host instead
+  host = 'localhost:3000' # Don't use this literally; use your local dev host instead
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 end
