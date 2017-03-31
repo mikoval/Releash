24961a2cbce179d685e95d2069d8b63e0287a108
Kamryn Harris <har.kamryn@gmail.com>
2017-03-30 22:23:11 -0500

    styling updates: login page, logo in nav bar, dashboard widgets updated

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module Releash
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components', 'gentelella', 'production')
     # set timezone
     config.time_zone = 'Georgetown'
   end
