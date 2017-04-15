1d5e4deb100e3423fdca82f7eba6b22769e26932
Natalie <castro.natalie94@gmail.com>
2017-04-15 01:34:19 -0400

    Made additions to User profile and bug fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+
 //= require jquery-ui
-//= require jquery_ujs
 //= require bootstrap
 //= require moment
 //= require bootstrap-datetimepicker
