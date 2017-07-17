a5d2ac0ac1608b198d4869e3fa554c9be5b196b6
Michael Koval <mkoval3@gatech.edu>
2017-07-17 08:16:38 -0400

    added move mobile features for dashboard

--- app/assets/javascripts/widgets.js
+++ app/assets/javascripts/widgets.js
@@ -1,3 +1,5 @@
+var gridly_timeout;
+var mobile = false;
 function activateWidgets(){
   widgetAlertList();
   widgetUserList();
@@ -36,16 +38,26 @@ function generateUsers(arr){
   }
   return str;
 }
+function initWidget(str){
+  console.log(mobile);
+  if(mobile){
+    return "<div class='panel-heading'> <h3 class= 'panel-title -leftpull'> "+str + "</h3></div>"
+  }
+  else{
+    return "<div class='panel-heading'> <h3 class= 'panel-title -leftpull'>"+str+"</h3><div class='right'><button type='button' class='remove-item-btn' onclick='return false;'><i class='lnr lnr-cross'></i></button></div></div>"
+  }
+}
 function widgetAlertList(){
     $.ajax({url: "/alerts/query", success: function(result){
-        var str = "<div class='panel-heading'> <h3 class= 'panel-title -leftpull'> Alerts</h3><div class='right'><button type='button' class='remove-item-btn' onclick='return false;'><i class='lnr lnr-cross'></i></button></div></div>"
+        
+        var str = initWidget("Alerts");
         str += "<div class='panel-body no-padding'> <div class='table-responsive'> <table class='table table-hover table-striped'> <thead> <tr> <th>Title</th> <th>Date</th> </tr> </thead>" 
         for (x in result){
     
-                str += "<tr> \
-                <td> <a href = '#' class = 'alert-event' id = 'alert-" + result[x].id + "'> " + result[x].title + "</a></td>\
-                 <td>" + result[x].date + "</td>\
-                </tr>"
+          str += "<tr> \
+          <td> <a href = '#' class = 'alert-event' id = 'alert-" + result[x].id + "'> " + result[x].title + "</a></td>\
+          <td>" + result[x].date + "</td>\
+          </tr>"
         }
         str +=  " </table> </div> <div class='text-right panel-footer'> <div class='row text-right'><a href= alerts class='btn btn-warning'>View all Alerts</a> </div></div>"
          $(".alert-list").each(function(){
@@ -56,7 +68,7 @@ function widgetAlertList(){
 }
 function widgetAnimalList(){
     $.ajax({url: "animals/querySimple", success: function(result){
-        var str = "<div class='panel-heading'> <h3 class= 'panel-title  '> Animals</h3><div class='right'><button type='button' class='remove-item-btn' onclick='return false;'><i class='lnr lnr-cross'></i></button></div></div>"
+        var str = initWidget("Animals");
         str += "<div class='panel-body no-padding'> <div class='table-responsive'> <table class='table table-hover table-striped'> <thead> <tr> <th>Name</th> <th>Primary Breed</th> <th>Age</th> </tr> </thead>" 
         for (x in result){
     
@@ -75,7 +87,7 @@ function widgetAnimalList(){
 }
 function widgetCoorList(){
     $.ajax({url: "animals/querySimpleCoor", success: function(result){
-        var str = "<div class='panel-heading'> <h3 class= 'panel-title  '> My Assigned Animals</h3><div class='right'><button type='button' class='remove-item-btn' onclick='return false;'><i class='lnr lnr-cross'></i></button></div></div>"
+        var str = initWidget("My Assigned Animals");
         str += "<div class='panel-body no-padding'> <div class='table-responsive'> <table class='table table-hover table-striped'> <thead> <tr> <th>Name</th> <th>Primary Breed</th> <th>Age</th> </tr> </thead>" 
         for (x in result){
     
@@ -94,7 +106,7 @@ function widgetCoorList(){
 }
 function widgetStatusList(){
     $.ajax({url: "animals/querySimpleStatus", success: function(result){
-        var str = "<div class='panel panel-scrolling'><div class='panel-heading'> <h3 class= 'panel-title  '> Status Count</h3><div class='right'><button type='button' class='remove-item-btn' onclick='return false;'><i class='lnr lnr-cross'></i></button></div></div>"
+        var str = initWidget("Status Count");
         str += "<div class='grid-stack-item-content panel-body'><div class='row'>" 
         for (x in result){
     
@@ -110,7 +122,7 @@ function widgetStatusList(){
 }
 function widgetUserList(){
     $.ajax({url: "people/query", success: function(result){
-        var str = "<div class='panel-heading'> <h3 class= 'panel-title'> Users</h3><div class='right'><button type='button' class='remove-item-btn' onclick='return false;'><i class='lnr lnr-cross'></i></button></div></div>"
+        var str = initWidget("User List");
         str += "<div class='panel-body no-padding'> <div class='table-responsive'> <table class='table table-hover table-striped'> <thead> <tr> <th>Name</th> <th>Email</th> </tr> </thead>" 
         for (x in result){
     
@@ -129,9 +141,9 @@ function widgetUserList(){
 function bindRemove(){
   $(".panel-heading").unbind('touchstart');
   $(".panel-heading").bind("touchstart", function(e){
-    console.log("click");
     if(!$(this).closest(".brick").hasClass("collapsed")){
         $(this).closest(".brick").find(".panel-body").hide();
+        $(this).closest(".brick").find(".panel-footer").hide();
         $(this).closest(".brick").addClass("collapsed");
         setTimeout(function(){$('.gridly').gridly();}, 500);
     }
@@ -145,8 +157,8 @@ function bindRemove(){
     }
    
   });
+  $(".remove-item-btn").unbind('click');
   $('.remove-item-btn').on('click', function(event) {
-    console.log("removing")
     $(this).closest('.brick').remove()
     saveLayout();
   })
@@ -155,64 +167,44 @@ function bindRemove(){
 
 function addAnimalWidget() {
   $('#widgetsList').modal('hide')   
-    var el = $.parseHTML('<div class = "brick large animal-list" ></div>');
+    var el = $.parseHTML('<div class = "brick large animal-list" widget-type="animal-list" ></div>');
 
     $(".gridly").append(el);
     widgetAnimalList()
-    $('.gridly').gridly(
-           {base: 350, // px 
-            gutter: 20, // px
-            columns: (parseInt( $(".content-body").width()/350))}
-          );
+    gridlyInit();
 }
 function addCoorWidget() {
   $('#widgetsList').modal('hide')   
-    var el = $.parseHTML('<div class = "brick large coor-list " ></div>');
+    var el = $.parseHTML('<div class = "brick large coor-list " widget-type="coor-list" ></div>');
 
     $(".gridly").append(el);
     widgetCoorList()
-    $('.gridly').gridly(
-           {base: 350, // px 
-            gutter: 20, // px
-            columns: (parseInt( $(".content-body").width()/350))}
-          );
+    gridlyInit();
 }
 function addStatusWidget() {
   $('#widgetsList').modal('hide')   
-    var el = $.parseHTML('<div class = "brick large status-list " ></div>');
+    var el = $.parseHTML('<div class = "brick large status-list " widget-type="status-list"></div>');
     $(".gridly").append(el);
     widgetStatusList()
-    $('.gridly').gridly(
-           {base: 350, // px 
-            gutter: 20, // px
-            columns: (parseInt( $(".content-body").width()/350))}
-          );
+    gridlyInit();
 }
 function addAlertsWidget() {
   $('#widgetsList').modal('hide')   
     
-    var el = $.parseHTML('<div class = "brick large alert-list " ></div>');
+    var el = $.parseHTML('<div class = "brick large alert-list " widget-type="alert-list"></div>');
     $(".gridly").append(el);
 
     widgetAlertList()
-    $('.gridly').gridly(
-           {base: 350, // px 
-            gutter: 20, // px
-            columns: (parseInt( $(".content-body").width()/350))}
-          );
+    gridlyInit();
 }
 function addUserWidget() {
   $('#widgetsList').modal('hide')   
-    var el = $.parseHTML('<div class = "brick large user-list" ></div>');
+    var el = $.parseHTML('<div class = "brick large user-list" widget-type="user-list"></div>');
 
     $(".gridly").append(el);
 
     widgetUserList()
-    $('.gridly').gridly(
-           {base: 350, // px 
-            gutter: 20, // px
-            columns: (parseInt( $(".content-body").width()/350))}
-          );
+    gridlyInit();
 }
 
 function loadWidgets(){
@@ -227,15 +219,12 @@ function loadWidgets(){
         
         for (i = 0; i < obj.length; i++){
           console.log(obj[i])
-          var el = $.parseHTML('<div class = "brick large ' + obj[i].type+' " >' + obj[i].type + '</div>');
+          var el = $.parseHTML('<div class = "brick large ' + obj[i].type+' " widget-type="'+obj[i].type+'">' + obj[i].type + '</div>');
           $(".gridly").append(el);
         }
-         $('.gridly').gridly(
-           {base: 350, // px 
-            gutter: 20, // px
-            columns: (parseInt( $(".content-body").width()/350))}
-          );
-         activateWidgets();
+        gridlyInit();
+        mobile=false;
+        activateWidgets();
        },
        error: function(){
         console.log("failed")
@@ -245,7 +234,6 @@ function loadWidgets(){
 
 function loadWidgetsMobile(){
 
-  console.log("loading widgets")
   $.ajax({
         url: "dashboard",
         type: "GET",
@@ -258,12 +246,14 @@ function loadWidgetsMobile(){
           var el = $.parseHTML('<div class = "brick mobile ' + obj[i].type+' " >' + obj[i].type + '</div>');
           $(".gridly").append(el);
         }
+        console.log($(window).width())
          $('.gridly').gridly(
-           {base: 350, // px 
-            gutter: 20, // px
+           {base: $(window).width(), // px 
+            gutter: 0, // px
             columns:1,
             draggable: false}
           );
+         mobile=true;
          activateWidgets();
        },
        error: function(){
@@ -275,5 +265,50 @@ function loadWidgetsMobile(){
 
 
 function saveLayout() {
-  
+  console.log('saving');
+  var arr = []
+    $(".brick").each(function() {
+        var type = $(this).attr("widget-type");
+        arr.push({"type": type});
+    })
+    console.log(arr);
+    var str = JSON.stringify(arr);
+    $.ajax({
+        url: "dashboard",
+        type: "PATCH",
+        data: {"str": str},
+       success: function(result){
+       },
+       error: function(){
+        console.log("failed")
+       }
+     })
 }
+function refreshGridly() {
+    var lastGridly = $('.gridly').clone();
+    
+    lastGridly.removeAttr('style');
+    $('.gridly-container').empty().append(lastGridly);
+
+    gridlyInit();
+    
+}
+function gridlyInit(){
+  console.log("here")
+  saveLayout()
+  $('.gridly').gridly(
+        {base: 350, // px 
+          gutter: 20, // px
+          columns: (parseInt( $(".content-body").width()/350)), 
+          callbacks: {
+              reordered: function(){saveLayout()}
+          }
+        }
+  );
+
+}
+$(window).resize(function(){
+    clearTimeout(gridly_timeout);
+    gridly_timeout = setTimeout(refreshGridly, 50);
+});
+
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,5 @@
 class MainController < ApplicationController
   before_filter :check_for_mobile
-    layout :resolve_layout
 
   def home
   end
